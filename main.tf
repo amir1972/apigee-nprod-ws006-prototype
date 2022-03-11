fa54649763de9560241cdd493754869f525f9820
Wayne Irwin <Wayne.Irwin@canadalife.com>
2022-03-11 13:00:29 -0600

    Missing variable

--- main.tf
+++ main.tf
@@ -1,10 +1,11 @@
 module "apigee-x-core" {
   source              = "github.com/apigee/terraform-modules//modules/apigee-x-core"
 
-  project_id        = var.project_id
-  ax_region         = var.ax_region
-  network           = var.network
-  apigee_instances  = var.apigee_instances
-  apigee_envgroups  = var.apigee_envgroups
+  project_id          = var.project_id
+  ax_region           = var.ax_region
+  network             = var.network
+  apigee_environments = var.apigee_environments
+  apigee_instances    = var.apigee_instances
+  apigee_envgroups    = var.apigee_envgroups
 
 }

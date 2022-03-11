78f9b4142d83c35aa06bca5a35e3fc59acff9cfe
Wayne Irwin <Wayne.Irwin@canadalife.com>
2022-03-11 12:17:24 -0600

    Simpler module

--- main.tf
+++ main.tf
@@ -1,16 +1,31 @@
-module "gcp_apigee_foundation" {
-  #source = "git::ssh://git@git.gwl.bz:7999/ecs/tflib-gcp-apigee-foundation.git"
-  source = "git::https://git.gwl.bz/scm/ecs/tflib-gcp-apigee-foundation.git"
+module "apigee-x-core" {
+  source              = "github.com/apigee/terraform-modules//modules/apigee-x-core"
 
-  project_id                    = var.project_id
-  deploy_region                 = var.deploy_region
-
-  ip_range                      = var.ip_range
-  network                       = var.network
-  analytics_region              = var.analytics_region
-  runtime_region                = var.runtime_region
-  apigee_environments           = var.apigee_environments
-  apigee_envgroups              = var.apigee_envgroups
-  cicd_cred_file                = var.cicd_cred_file
-  terraform_service_account     = var.terraform_service_account
+  project_id                = "apigee-nprod-ws004-prototype"
+  #IP Ranges documented at https://spaces.gwl.ca/display/ECS/GCP+Projects+and+CIDR+Implementation
+  ax_region         = "us-east1"  #Analytics Region (and also KeyRing for Database Encryption)
+  network           = "apigee-nprod-prototype-vpc"
+  #network          = " int-cl-apigee-prototype-shared-vpc-1"
+  apigee_instances  = {
+    # Single instance only for eval, add a second instance for prod setups
+    na-ne1-instance = {
+      region        = "northamerica-northeast1"  #And also for Disk Encryption Keyring
+      ip_range      = "10.58.24.0/22"
+      environments  = ["proto1", "proto2"]
+    }
+    # Example of second instance
+    # na-ne2-instance = {
+    #   region        = "europe-west2"
+    #   ip_range      = "10.0.8.0/22"
+    #   environments  = ["test1", "test2"]
+    # }
+  }
+  apigee_envgroups    = {
+                          prototype = {
+                            environments = ["proto1", "proto2"]
+                            hostnames    = ["prototype-apix.canadalife.com"]
+                          }
+                        }
+  cicd_cred_file             = "apigee-nprod-ws004-prototype.json"
+  terraform_service_account  = "apigee-nprod-ws004-tf-prod@cantech-terraformers.iam.gserviceaccount.com"
 }
--- terraform.tfvars
+++ terraform.tfvars
@@ -1,17 +1,1 @@
-project_id                = "apigee-nprod-ws004-prototype"
-deploy_region             = "northamerica-northeast1"
-#IP Ranges documented at https://spaces.gwl.ca/display/ECS/GCP+Projects+and+CIDR+Implementation
-ip_range                  = {range-a = "10.58.24.0/22"}  #Assumed second range of /cidr 28 added to end
-network                   = "apigee-nprod-prototype-vpc"
-#network                   = "projects/net-hub-infra/global/networks/int-cl-apigee-prototype-shared-vpc-1"
-analytics_region          = "us-east1"
-runtime_region            = "northamerica-northeast1"
-apigee_environments       = ["proto1", "proto2"]
-apigee_envgroups          = {
-                              prototype = {
-                                environments = ["proto1", "proto2"]
-                                hostnames    = ["prototype-apix.canadalife.com"]
-                              }
-                            }
-cicd_cred_file             = "apigee-nprod-ws004-prototype.json"
-terraform_service_account  = "apigee-nprod-ws004-tf-prod@cantech-terraformers.iam.gserviceaccount.com"
+#In mian.tf

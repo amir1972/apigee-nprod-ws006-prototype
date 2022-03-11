95f0312e0e6fc656fef16a12e8af64c58e1f6875
Wayne Irwin <Wayne.Irwin@canadalife.com>
2022-03-11 12:46:15 -0600

    Next step

--- terraform.tfvars
+++ terraform.tfvars
@@ -1,1 +1,26 @@
-#In mian.tf
+project_id                = "apigee-nprod-ws004-prototype"
+  #IP Ranges documented at https://spaces.gwl.ca/display/ECS/GCP+Projects+and+CIDR+Implementation
+  ax_region         = "us-east1"  #Analytics Region (and also KeyRing for Database Encryption)
+  network           = "int-cl-apigee-prototype-shared-vpc-1"
+  apigee_instances  = {
+    # Single instance only for eval, add a second instance for prod setups
+    na-ne1-instance = {
+      region        = "northamerica-northeast1"  #And also for Disk Encryption Keyring
+      ip_range      = "10.58.24.0/22"
+      environments  = ["proto1", "proto2"]
+    }
+    # Example of second instance
+    # na-ne2-instance = {
+    #   region        = "europe-west2"
+    #   ip_range      = "10.0.8.0/22"
+    #   environments  = ["test1", "test2"]
+    # }
+  }
+  apigee_envgroups    = {
+                          prototype = {
+                            environments = ["proto1", "proto2"]
+                            hostnames    = ["prototype-apix.canadalife.com"]
+                          }
+                        }
+  cicd_cred_file             = "apigee-nprod-ws004-prototype.json"
+  terraform_service_account  = "apigee-nprod-ws004-tf-prod@cantech-terraformers.iam.gserviceaccount.com"
--- variables.tf
+++ variables.tf
@@ -1,8 +1,18 @@
 // variables.tf
 
-variable "analytics_region" {
+variable "project_id" {
+  description = "Project ID to host this Apigee organization (will also become the Apigee Org name)."
+  type        = string
+}
+
+variable "ax_region" {
   description = "Analytics Region for the Apigee Organization (immutable). See https://cloud.google.com/apigee/docs/api-platform/get-started/install-cli."
   type = string
+}
+
+variable "network" {
+  description = "Name of the VPC network to be created."
+  type        = string
 }
 
 variable "deploy_region" {
@@ -25,19 +35,14 @@ variable "apigee_environments" {
   default     = []
 }
 
-variable "network" {
-  description = "Name of the VPC network to be created."
-  type        = string
-}
-
-variable "project_id" {
-  description = "Project ID to host this Apigee organization (will also become the Apigee Org name)."
-  type        = string
-}
-
-variable "runtime_region" {
-  description = "Apigee Runtime Instance Region."
-  type        = string
+variable "apigee_instances" {
+  description = "Apigee Instances (only one instance for EVAL)."
+  type = map(object({
+    region       = string
+    ip_range     = string
+    environments = list(string)
+  }))
+  default = {}
 }
 
 variable "terraform_service_account" {
@@ -48,26 +53,4 @@ variable "terraform_service_account" {
 variable "cicd_cred_file" {
   description = "Impersonation key"
   type = string
-}
-
-  # variable "ip_range" {
-  #   description = "Customer-provided CIDR block of length 22 for the Apigee instance."
-  #   type = string
-  #   validation {
-  #     condition = try(cidrnetmask(var.ip_range), null) == "255.255.252.0"
-  #     error_message = "Invalid CIDR block provided; Allowed pattern for ip_range: X.X.X.X/22."
-  #   }
-  # }
-
-variable "ip_range" {
-  description = "CIDR ranges used for Google services that support Private Service Networking."
-  type        = map(string)
-  default     = null
-  validation {
-    condition = alltrue([
-      for k, v in(var.ip_range == null ? {} : var.ip_range) :
-      can(cidrnetmask(v))
-    ])
-    error_message = "Specify valid RFC1918 CIDR ranges for Private Service Networking."
-  }
 }

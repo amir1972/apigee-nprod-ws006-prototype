5134b0e0308786e2e23e0514a9c6ca723e7b06cd
Wayne Irwin <Wayne.Irwin@canadalife.com>
2022-03-30 14:56:47 -0500

    remove deplicate variable

--- variables.tf
+++ variables.tf
@@ -532,14 +532,14 @@ variable "logging_sinks" {
 }
 
 
-variable "organization_id" {
-  description = "Organization id in organizations/nnnnnn format."
-  type        = string
-  validation {
-    condition     = can(regex("^organizations/[0-9]+", var.organization_id))
-    error_message = "The organization_id must in the form organizations/nnn."
-  }
-}
+#variable "organization_id" {
+#  description = "Organization id in organizations/nnnnnn format."
+#  type        = string
+#  validation {
+#    condition     = can(regex("^organizations/[0-9]+", var.organization_id))
+#    error_message = "The organization_id must in the form organizations/nnn."
+#  }
+#}
 
 
 variable "policy_boolean" {

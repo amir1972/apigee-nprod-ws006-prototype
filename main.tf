59cbd498b9eedca4d4d66dc4ac1f11bc674bfbdc
hasantayyar <tayyar.besik@gmail.com>
2022-05-02 16:28:21 +0200

    Add ilb module

--- main.tf
+++ main.tf
@@ -135,3 +135,28 @@ module "apigee-x-mtls-mig" {
   subnet        = var.apigee_mtls_subnet # subnet to be passed statically via variables
   region        = each.value.region
 }
+
+module "ilb" {
+  for_each      = var.apigee_instances
+  source        = "github.com/terraform-google-modules/cloud-foundation-fabric//modules/net-ilb?ref=v15.0.0"
+  project_id    = var.project_id
+  region        = each.value.region
+  name          = "apigee-mtls-${each.key}"
+  service_label = "apigee-mtls-${each.key}"
+  network       = var.apigee_mtls_network
+  subnetwork    = var.apigee_mtls_subnet
+  ports         = [443]
+  backends = [
+    {
+      group          = module.apigee-x-mtls-mig[each.key].instance_group,
+      failover       = false
+      balancing_mode = "CONNECTION"
+    }
+  ]
+  health_check_config = {
+    type    = "tcp"
+    check   = { port = 443 }
+    config  = {}
+    logging = true
+  }
+}
